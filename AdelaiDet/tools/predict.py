56dfd613e978a2c9207742c3e4379ab6e7935aed
Nguyen Tien Hung <33962566+hungnt14@users.noreply.github.com>
2021-12-16 10:53:56 +0700

    Update predict.py

--- AdelaiDet/tools/predict.py
+++ AdelaiDet/tools/predict.py
@@ -1,7 +1,6 @@
 from detectron2.engine.defaults import DefaultPredictor
 from detectron2.data.detection_utils import read_image
 from adet.config import get_cfg
-from tqdm import tqdm_notebook
 import time
 import glob
 import os
@@ -28,74 +27,6 @@ def get_parser():
     )
     return parser
 
-dictionary = "aàáạảãâầấậẩẫăằắặẳẵAÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪeèéẹẻẽêềếệểễEÈÉẸẺẼÊỀẾỆỂỄoòóọỏõôồốộổỗơờớợởỡOÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠiìíịỉĩIÌÍỊỈĨuùúụủũưừứựửữƯỪỨỰỬỮUÙÚỤỦŨyỳýỵỷỹYỲÝỴỶỸ"
- 
- 
-def make_groups():
-    # dictionary = 'aàáạảãâầấậẩẫăằắặẳẵAÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪeèéẹẻẽêềếệểễEÈÉẸẺẼÊỀẾỆỂỄoòóọỏõôồốộổỗơờớợởỡOÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠiìíịỉĩIÌÍỊỈĨuùúụủũưừứựửữƯỪỨỰỬỮUÙÚỤỦŨyỳýỵỷỹYỲÝỴỶỸ'
-    groups = []
-    i = 0
-    while i < len(dictionary) - 5:
-        group = [c for c in dictionary[i : i + 6]]
-        i += 6
-        groups.append(group)
-    return groups
- 
-
-groups = make_groups()
- 
-TONES = ["", "ˋ", "ˊ", "﹒", "ˀ", "˜"]
-SOURCES = ["ă", "â", "Ă", "Â", "ê", "Ê", "ô", "ơ", "Ô", "Ơ", "ư", "Ư", "Đ", "đ"]
-TARGETS = ["aˇ", "aˆ", "Aˇ", "Aˆ", "eˆ", "Eˆ", "oˆ", "o˒", "Oˆ", "O˒", "u˒", "U˒", "D‑", "d‑"]
-
-def correct_tone_position(word):
-    word = word[:-1]
-    if len(word) < 2:
-        pass
-    first_ord_char = ""
-    second_order_char = ""
-    for char in word:
-        for group in groups:
-            if char in group:
-                second_order_char = first_ord_char
-                first_ord_char = group[0]
-    if word[-1] == first_ord_char and second_order_char != "":
-        pair_chars = ["qu", "Qu", "qU", "QU", "gi", "Gi", "gI", "GI"]
-        for pair in pair_chars:
-            if pair in word and second_order_char in ["u", "U", "i", "I"]:
-                return first_ord_char
-        return second_order_char
-    return first_ord_char
-  
-def decoder(recognition):
-    for char in TARGETS:
-        recognition = recognition.replace(char, SOURCES[TARGETS.index(char)])
-    if len(recognition) < 1:
-        return recognition
-    if recognition[-1] in TONES:
-        if len(recognition) < 2:
-            return recognition
-        replace_char = correct_tone_position(recognition)
-        tone = recognition[-1]
-        recognition = recognition[:-1]
-        for group in groups:
-            if replace_char in group:
-                recognition = recognition.replace(replace_char, group[TONES.index(tone)])
-    return recognition
- 
-def _decode_recognition(rec):
-    #        CTLABELS = [' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~']
-    # CTLABELS = ['^', '\\', '}', 'ỵ', '>', '<', '{', '~', '`', '°', '$', 'ẽ', 'ỷ', 'ẳ', '_', 'ỡ', ';', '=', 'Ẳ', 'j', '[', ']', 'ẵ', '?', 'ẫ', 'Ẵ', 'ỳ', 'Ỡ', 'ẹ', 'è', 'z', 'ỹ', 'ằ', 'õ', 'ũ', 'Ẽ', 'ỗ', 'ỏ', '@', 'Ằ', 'Ỳ', 'Ẫ', 'ù', 'ử', '#', 'Ẹ', 'Z', 'Õ', 'ĩ', 'Ỏ', 'È', 'Ỷ', 'ý', 'Ũ', '*', 'ò', 'é', 'q', 'ở', 'ổ', 'ủ', 'ẩ', 'ã', 'ẻ', 'J', 'ữ', 'ễ', 'ặ', '+', 'ứ', 'Ỹ', 'ự', 'ụ', 'Ỗ', '%', 'ắ', 'ồ', '"', 'ề', 'ể', 'ỉ', 'ợ', '!', 'Ẻ', 'ừ', 'ọ', '&', 'ì', 'É', 'ậ', 'Ù', 'Ặ', 'x', 'Ỉ', 'ú', 'í', 'ó', 'Ẩ', 'ị', 'ế', 'Ứ', 'â', 'ấ', 'ầ', 'ớ', 'ă', 'Ủ', 'Ĩ', '(', 'Ắ', 'Ừ', ')', 'ờ', 'Ý', 'Ễ', 'Ã', 'ô', 'ộ', 'Ữ', 'Ợ', 'ả', 'Ở', 'ệ', 'W', 'ơ', 'Ổ', 'ố', 'Ề', 'f', 'Ử', 'ạ', 'w', 'Ò', 'Ự', 'Ụ', 'Ú', 'Ồ', 'ê', 'Ó', 'Ì', 'b', 'Í', 'Ể', 'đ', 'Ớ', '/', 'k', 'Ă', 'v', 'Ị', 'Ậ', 'Ọ', 'd', 'Ầ', 'Ấ', 'ư', 'á', 'Ế', ' ', 'p', 'Ơ', 'F', 'Ả', 'Ộ', 'Ê', 'Ờ', 's', '-', 'à', 'y', 'Ố', 'l', 'Â', 'Q', ',', 'X', 'Ệ', 'Ạ', 'Ô', 'r', ':', '6', '7', 'u', '4', 'm', '5', 'e', '8', 'c', 'Ư', 'Á', '9', 'D', '3', 'o', '.', 'Y', 'g', 'K', 'a', 'À', 't', '2', 'B', 'E', 'V', 'R', '1', 'S', 'i', 'L', 'P', 'Đ', 'h', 'U', '0', 'M', 'O', 'n', 'A', 'G', 'I', 'C', 'T', 'H', 'N']
-    CTLABELS = [' ','!','"','#','$','%','&',"\'",'(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','ˋ','ˊ','﹒','ˀ','˜','ˇ','ˆ','˒','‑']
-    s = ""
-    for c in rec:
-        c = int(c)
-        if c < 104:
-            s += CTLABELS[c]
-        elif c == 104:
-            s += u"口"
-    return decoder(s)
- 
 def prepare_cfg(args):
     # load config from file and command-line arguments
     cfg = get_cfg()
@@ -119,13 +50,14 @@ if __name__ == "__main__":
     predictor = DefaultPredictor(cfg)
     
     print('Read predictor successfully. We are ready for predictions')
+    
     list_txts = []
     start_time = time.time()
     files = glob.glob(args.input + "*")
-    
+    countBbox = 0
     trace = {}
-    cnt = 0
-    for img_path in files:
+    
+    for id, img_path in enumerate(files):
         img = read_image(img_path, format="BGR")
         filename = os.path.basename(img_path)
         predictions = predictor(img)
@@ -138,13 +70,12 @@ if __name__ == "__main__":
         for p, rec in zip(beziers, recs):
             p = [list(map(int,p[i:i + 2])) for i in range(0, len(p), 2)]
             points = [p[0]] + [p[3]] + [p[4]] + [p[7]]
-            text = _decode_recognition(rec)
-            text = ""
             for _, _p in enumerate(points):
                 content += ','.join(list(map(str,list(map(int,_p)))))
                 if not (_ == len(points) - 1):
                     content += ','
             content += "\n"
+            countBbox += 1
         
         if filename in trace:
             print("nope", filename)
@@ -154,6 +85,7 @@ if __name__ == "__main__":
             f_submission = open(args.output + filename + ".txt", 'w', encoding="utf-8")
             f_submission.write(content)
             f_submission.close()
-            cnt += 1
-            print("Done", filename, " - " + str(cnt) + "/" + str(len(files)))
+            print("Done", filename, "-", str(id) + "/" + str(len(files)))
+    end_time = time.time()
     
+    print("============ FINISHED DETECTION (time elapsed: {}). TOTAL DETECTED BBOX: {} ============".format(str(countBbox), str(end_time - start_time)))
